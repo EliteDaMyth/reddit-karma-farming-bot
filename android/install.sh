e9c37136b114c2c44ad83556fdca6d1c394564a9
Elite Myth <elitemythyt@gmail.com>
2019-04-22 16:44:52 +0530

    Add files via upload

---
+++ android/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+apt install nano
+apt install git
+apt install python2
+apt install python-dev
+apt install python-setuptools
+apt install sqlite
+apt install libsqllite-dev
+apt install libffi-dev
+python2 install -r requirements.txt
+echo "Everything Now Installed Please setup your vredentials in reddit.py using nano reddit.py then run the bot using python2 run.py"
